c1a14557140462658f840733a76979f6b0d3fc93
Suemin Cho <sm991105@gmail.com>
2021-03-11 18:12:31 +0900

    working on menu tab

--- app/src/main/java/com/example/binding/src/main/menu/MenuFragment.kt
+++ app/src/main/java/com/example/binding/src/main/menu/MenuFragment.kt
@@ -2,6 +2,7 @@ package com.example.binding.src.main.menu
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import com.example.binding.R
 import com.example.binding.config.BaseFragment
 import com.example.binding.databinding.FragmentMenuBinding
